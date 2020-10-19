87e0545d83900475774af3033fb9e53edde70eed
Abla-Adem <47026846+Abla-Adem@users.noreply.github.com>
2020-10-20 01:49:41 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -9,14 +9,15 @@ ty=[]
 surf=0
 surface=[]
 n=100000
-r = np.sqrt(2.0)
-while(abs(6.28318-surf)>0.001):
+r = np.sqrt(8)
+bor=3
+while(abs((r*r*math.pi)-surf)>0.001):
 
     n=n+100000
     surf=0
     for i in range(0, n):
-        tempx = random.uniform(-2, 2)
-        tempy = random.uniform(-2, 2)
+        tempx = random.uniform(-bor,bor)
+        tempy = random.uniform(-bor,bor)
 
         t.append(tempx)
         ty.append(tempy)
@@ -24,10 +25,13 @@ while(abs(6.28318-surf)>0.001):
         if ((r >= t[len(t) - 1] or -r <= t[len(t) - 1]) and (
                 (math.pow(t[len(t) - 1], 2) + math.pow(ty[len(ty) - 1], 2)) <= (r * r))):
             surf += 1
-    surf=(surf / n) * 4 * 4
+    surf=(surf / n) * bor*2 * bor*2
     surface.append(surf)
-print(surface)
-"""x1 = r*np.cos(theta)
+    print(abs((r*r*math.pi)-surf))
+print(surface,r*r*math.pi)
+
+"""
+x1 = r*np.cos(theta)
 x2 = r*np.sin(theta)
 print((surf/n)*4*4)
 fig, ax = plt.subplots(1)
@@ -45,5 +49,5 @@ plt.title('How to plot a circle with matplotlib ?', fontsize=8)
 
 plt.savefig("plot_circle_matplotlib_01.png", bbox_inches='tight')
 
-plt.show()"""
-
+plt.show()
+"""
